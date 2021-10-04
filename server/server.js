bc6b4efd45353647e8d2f780857903e72dd8ef2f
hxckey <george.h@live.com>
2021-10-04 16:10:30 +0100

    add initial tests and app routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.port || 3000;
+const port = 5000;
 
 app.use(cors());
 app.listen(port, () =>{
     console.log(`app listening at https://localhost:${port}`)
 });
+
+app.get("/", (req, res) => res.send("Welcome to StreetHub"));
+
+app.get("/articles", (req, res) => res.json({results: articles}));
+
+app.get("/articles/:id", (req, res) => {
+    if(req.params.id > articles.length || req.params.id < 0){
+    res.send('Please enter a number greater than 0.');
+  } else {
+    res.send(articles[req.params.id-1])
+  }})
+
+module.exports = app
+
+const articles = ['hello', 'world']

6b130c52b0e84ddd30fb83ebac6e5504a89eede3
hxckey <george.h@live.com>
2021-10-05 11:54:06 +0100

    add testing for all routes

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -6,13 +6,13 @@ const app = require('community_journaling/server/server.js')
 describe('API routes', () => {
     let api;
 
-    beforeEach(() => {
+    beforeAll(() => {
         api = app.listen(3000, () => {
             console.log('Server running on port 3000')
         });
     });
 
-    afterEach(done => {
+    afterAll(done => {
         console.log('Stopping test server')
         api.close(done);
     });
@@ -44,8 +44,9 @@ describe('API routes', () => {
 
     test('test post path for comments', () => {
         return superT(api)
-        .post('/newcomment')
+        .post("/newcomment")
         .expect(201)
+        .expect('Comment posted')
     })
-    
+
 });
